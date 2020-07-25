6096a8d63e704e1dba6b089a134d285c8c8b1990
Michael Cohen <scudette@gmail.com>
2020-07-25 23:29:53 +1000

    2

--- bin/binary_test.go
+++ bin/binary_test.go
@@ -86,13 +86,15 @@ func (self *MainTestSuite) TestAutoexec() {
 
 	// Repack the config in the binary.
 	cmd := exec.Command(self.binary, "config", "repack", config_file.Name(), exe.Name())
-	out, err := cmd.Output()
+	out, err := cmd.CombinedOutput()
+	fmt.Printf("Stdout: %v", out)
 	require.NoError(self.T(), err)
 
 	// Run the repacked binary with no args - it should run the
 	// `artifacts list` command.
 	cmd = exec.Command(exe.Name())
-	out, err = cmd.Output()
+	out, err = cmd.CombinedOutput()
+	fmt.Printf("Stdout: %v", out)
 	require.NoError(self.T(), err)
 
 	// The output should contain MySpecialArtifact as well as the
@@ -182,7 +184,8 @@ func (self *MainTestSuite) TestGenerateConfigWithMerge() {
 
 	// Repack the config in the binary.
 	cmd = exec.Command(self.binary, "config", "repack", config_file.Name(), exe.Name())
-	out, err = cmd.Output()
+	out, err = cmd.CombinedOutput()
+	fmt.Printf("Stdout: %v", out)
 	require.NoError(self.T(), err)
 
 	// Run the repacked binary with invalid environ - config
