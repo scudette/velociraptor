b900828ec7059859a05a8d62bcc76ebc47219bb2
Michael Cohen <scudette@gmail.com>
2020-09-27 19:48:36 +1000

    Try to build react app in pipeline

--- gui/velociraptor/src/index.js
+++ gui/velociraptor/src/index.js
@@ -13,11 +13,30 @@ import * as serviceWorker from './serviceWorker';
 import {HashRouter} from "react-router-dom";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faCrosshairs, faWrench, faEye, faServer, faBook, faLaptop,
-         faSearch, faSpinner,  faSearchPlus, faFolderOpen, faHistory,
-         faTasks, faTerminal, faCompress, faExpand, faEyeSlash, faStop, faExclamation,
-         faColumns, faDownload,
-         faBinoculars } from '@fortawesome/free-solid-svg-icons';
+
+import faHome from '@fortawesome/free-solid-svg-icons/faHome.js';
+import faCrosshairs from '@fortawesome/free-solid-svg-icons/faCrosshairs.js';
+import faWrench from '@fortawesome/free-solid-svg-icons/faWrench.js';
+import faEye from '@fortawesome/free-solid-svg-icons/faEye.js';
+import faServer from '@fortawesome/free-solid-svg-icons/faServer.js';
+import faBook from '@fortawesome/free-solid-svg-icons/faBook.js';
+import faLaptop from '@fortawesome/free-solid-svg-icons/faLaptop.js';
+import faSearch from '@fortawesome/free-solid-svg-icons/faSearch.js';
+import faSpinner from '@fortawesome/free-solid-svg-icons/faSpinner.js';
+import faSearchPlus from '@fortawesome/free-solid-svg-icons/faSearchPlus.js';
+import faFolderOpen from '@fortawesome/free-solid-svg-icons/faFolderOpen.js';
+import faHistory from '@fortawesome/free-solid-svg-icons/faHistory.js';
+import faTasks from '@fortawesome/free-solid-svg-icons/faTasks.js';
+import faTerminal from '@fortawesome/free-solid-svg-icons/faTerminal.js';
+import faCompress from '@fortawesome/free-solid-svg-icons/faCompress.js';
+import faExpand from '@fortawesome/free-solid-svg-icons/faExpand.js';
+import faEyeSlash from '@fortawesome/free-solid-svg-icons/faEyeSlash.js';
+import faStop from '@fortawesome/free-solid-svg-icons/faStop.js';
+import faExclamation from '@fortawesome/free-solid-svg-icons/faExclamation.js';
+import faColumns from '@fortawesome/free-solid-svg-icons/faColumns.js';
+import faDownload from '@fortawesome/free-solid-svg-icons/faDownload.js';
+import faBinoculars from '@fortawesome/free-solid-svg-icons/faBinoculars.js';
+
 
 library.add(faHome, faCrosshairs, faWrench, faEye, faServer, faBook, faLaptop,
             faSearch, faSpinner, faSearchPlus, faTasks, faTerminal,
